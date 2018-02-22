3584fcf08fe296a38ace97f99c25f0879a721b0a
matheusgeres <matheusgeres@gmail.com>
2018-02-22 00:18:23 -0300

    Finalização da migração da aplicação para React + Redux.

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 class TodoForm extends Component {
     constructor(props) {
@@ -17,11 +17,11 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
-        const { add, search, description } = this.props
+        const { add, clear, search, description } = this.props
         if (e.key === 'Enter'){
             e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
-            props.handleClear()
+            clear()
         }
     }
     
@@ -41,9 +41,9 @@ class TodoForm extends Component {
                     <IconButton style='primary' icon='plus'
                         onClick={() => add(description)}></IconButton>
                     <IconButton style='info' icon='search'
-                        onClick={() => search()}></IconButton>
+                        onClick={search}></IconButton>
                     <IconButton style='default' icon='close'
-                        onClick={this.props.handleClear}></IconButton>
+                        onClick={this.props.clear}></IconButton>
                 </Grid>
             </div>
         )
@@ -51,5 +51,5 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
-const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
